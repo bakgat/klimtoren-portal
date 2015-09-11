6ed8fee8697a40c67cfbdd47661b0330511b4e92
bakgat <karl.vaniseghem@klimtoren.be>
2015-09-11 19:46:58 +0200

    Dutch default

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -34,7 +34,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Be right back.</div>
+				<div class="title">We zijn zo terug.</div>
 			</div>
 		</div>
 	</body>
